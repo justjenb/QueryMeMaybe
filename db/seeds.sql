229fe1cfa931990a88655e737ab4fd549302ea1a
Jen B <jenbutler984@gmail.com>
2023-06-06 19:38:17 -0400

    Initial commit, added file structure folders/files, added helpers and middleware from previous repos to assist

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (name)
+VALUES ("Customer Service"),
+       ("Sales"),
+       ("Operations"),
+       ("Projects");
+
+INSERT INTO role (title, salary)
+VALUES ("Customer Service Manager", 70000),
+       ("Sales Manager", 80000),
+       ("Operations Manager", 80000),
+       ("Project Manager", 60000);
+
+INSERT INTO employee (first_name, last_name)
+VALUES ("Michael", "Ferrara"),
+       ("John", "Audi"),
+       ("Alan", "Olson"),
+       ("Ryan", "Shapiro");
